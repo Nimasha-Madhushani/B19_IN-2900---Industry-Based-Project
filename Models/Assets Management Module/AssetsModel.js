15a6c9945773b27b75c54d787b8e32b29b3ef6d5
kanchanakariyawasam98 <kanchanakariyawasam98@gmail.com>
2022-01-26 19:54:54 +0530

    create assets and assetlender models

---
+++ Models/Assets Management Module/AssetsModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const assetsSchema = new Schema({
+    assetID :{
+        type : String,
+        required : true
+    },
+    assetCategory :{
+        type : String,
+        required : true
+    },
+    model :{
+        type : String,
+        required : true
+    },
+    serialNumber : {
+        type : String,
+        required : true
+    },
+    status :{
+        type : String,
+        required : true,
+        default : 'Available'
+    }
+});
+
+const Asset = mongoose.model("Asset",assetsSchema);
+module.exports = Asset;
