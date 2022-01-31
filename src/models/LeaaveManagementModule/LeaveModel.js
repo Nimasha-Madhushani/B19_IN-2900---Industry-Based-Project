5900f1f5628f98650c17d0bfd2f02d168d29f227
Rashmi Gamage <wgrashmi98@gmail.com>
2022-01-31 23:21:21 +0530

    update leave model and create initial routes

--- src/models/LeaaveManagementModule/LeaveModel.js
+++ src/models/LeaaveManagementModule/LeaveModel.js
@@ -19,6 +19,8 @@ const leaveSchema = new mongoose.Schema ({
     },
     status : {
         type : String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: "pending"
         
     },
     leaveMethod : {
