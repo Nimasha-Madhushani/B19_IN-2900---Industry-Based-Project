d03601afc477ee3a99ebde9dfab9fcbf8bae6962
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-01-31 15:02:15 +0530

    separate routs to seperated controllers

--- src/routes/ReportersManagementModule/employeeRoutes.js
+++ src/routes/ReportersManagementModule/employeeRoutes.js
@@ -1,33 +1,49 @@
 const router = require("express").Router();
+
+//----------------employee controller------------------------------------------
 const {
-createEmployee,addProduct, addTeam, viewEmployees, viewTeam, updateEmployeeProfile, updateProduct, updateTeam
+  createEmployee,
+  viewEmployees,
+  updateEmployeeProfile,
 } = require("../../controllers/ReportersSection/employeeControllers");
 
 //view all employees
-router.get('/',viewEmployees);
+router.get("/", viewEmployees);
 
 //create employee
-router.post('/add', createEmployee);
+router.post("/add", createEmployee);
 
 //employee profile update including academic/proffesional qualifications
-router.put('/update/:id',updateEmployeeProfile);
+router.put("/update/:id", updateEmployeeProfile);
+
+//----------------------teamController------------------------------------------
+
+const {
+  addTeam,
+  viewTeam,
+  updateTeam,
+} = require("../../controllers/ReportersSection/teamControllers");
 
 //view all teams
-router.get('/viewTeam',viewTeam);
+router.get("/viewTeam", viewTeam);
 
 //add team
-router.post('/teamAdd',addTeam);
+router.post("/teamAdd", addTeam);
 
 //update team
-router.put('/updateTeam/:id',updateTeam);
+router.put("/updateTeam/:id", updateTeam);
+
+//------------------------productController---------------------------------------
+
+const {
+  addProduct,
+  updateProduct,
+} = require("../../controllers/ReportersSection/productConotrollers");
 
 //add product
-router.post('/addProduct',addProduct);
+router.post("/addProduct", addProduct);
 
 //update product
-router.post('/updateProduct/:id',updateProduct);
-
-
-
+router.post("/updateProduct/:id", updateProduct);
 
 module.exports = router;
