ba6142a608cbd5a4fb50bf1cf5296f3b6af42485
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-02-04 21:41:24 +0530

    create recent section controller

---
+++ src/controllers/ReportersSection/recentSectionController.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const employeeSchema = require("../../models/ReportersManagementModule/EmployeeModel");
+module.exports.recentSection = async (req, res) => {
+  try {
+    const Count = await employeeSchema.count();
+
+    const recent = await employeeSchema.find().skip(Count <= 5 ? 0 : Count - 5);
+    res.status(200).json({ state: true, data: recent });
+  } catch {
+    res.status(400).json({ state: false });
+  }
+};
+
+
+/*
+
+//------------create organization structure---------------------------------
+
+*/
+
