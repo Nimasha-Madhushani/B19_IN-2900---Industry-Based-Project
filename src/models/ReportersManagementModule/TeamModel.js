d8c1b5e2215de5b0f22aa833b78bb18486e88345
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-02-04 21:44:51 +0530

    changes in team model

--- src/models/ReportersManagementModule/TeamModel.js
+++ src/models/ReportersManagementModule/TeamModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema(
   {
-    teamID: {
+    /*teamID: {
       type: String,
       required: true,
-    },
+    },*/
     teamName: {
       type: String,
       required: true,
@@ -14,6 +14,10 @@ const teamSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    //--------------------
+   /* teamMembers:{
+      type:Array
+    }*/
   },
   { timestamps: true }
 );
