31ac125ff72f7c1d3f9b1fe016050c008d478f05
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-02-05 10:44:25 +0530

    update employee routes

--- src/routes/ReportersManagementModule/employeeRoutes.js
+++ src/routes/ReportersManagementModule/employeeRoutes.js
@@ -1,24 +1,10 @@
 const router = require("express").Router();
 
-//----------------employee controller------------------------------------------
 const {
   createEmployee,
   viewEmployees,
   updateEmployeeProfile,
 } = require("../../controllers/ReportersSection/employeeControllers");
-
-//view all employees
-router.get("/", viewEmployees);
-
-//create employee
-router.post("/add", createEmployee);
-
-//employee profile update including academic/proffesional qualifications
-router.put("/update/:id", updateEmployeeProfile);
-
-//------------------------------------------------------------------------------
-
-//----------------------teamController------------------------------------------
 
 const {
   addTeam,
@@ -26,33 +12,42 @@ const {
   updateTeam,
 } = require("../../controllers/ReportersSection/teamControllers");
 
-//view all teams
+const {
+  addProduct,
+  updateProduct,
+  viewProducts,
+} = require("../../controllers/ReportersSection/productConotrollers");
+
+const {
+  recentSection,
+} = require("../../controllers/ReportersSection/recentSectionController");
+
+//----------------employee controller------------------------------------------
+
+router.get("/", viewEmployees);
+
+router.post("/add", createEmployee);
+
+router.put("/update/:id", updateEmployeeProfile);
+
+//----------------------teamController------------------------------------------
+
 router.get("/viewTeam", viewTeam);
 
-//add team
 router.post("/teamAdd", addTeam);
 
-//update team
 router.put("/updateTeam/:id", updateTeam);
 
 //------------------------productController---------------------------------------
 
-const {
-  addProduct,
-  updateProduct,
-} = require("../../controllers/ReportersSection/productConotrollers");
-
-//add product
 router.post("/addProduct", addProduct);
 
-//update product
 router.post("/updateProduct/:id", updateProduct);
 
+router.get("/viewProducts", viewProducts);
 
-//--------recent section-----------
-const {
-  recentSection,
-} = require("../../controllers/ReportersSection/recentSectionController");
+//--------recent section--------------------------------------------------------
+
 router.get("/recentSection", recentSection);
 
 module.exports = router;
