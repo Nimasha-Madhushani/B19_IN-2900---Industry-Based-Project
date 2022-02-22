1a46f9ce2f4ad9131e7bdaf2ee83fb3a2a67bac6
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-02-22 11:36:39 +0530

    create last seen controller

---
+++ src/controllers/ReportersSection/lastSeenController.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const employeeSchema = require("../../models/ReportersManagementModule/EmployeeModel");
+
+//------------update last seen of employees-----------------------
+
+//-------------view last seen of employees------------------------
+exports.displayLastSeen = async (req, res) => {
+  try {
+    const findEmployees = await employeeSchema.find();
+    let lastSeenArr = new Array();
+
+    await Promise.all(
+      findEmployees.map(async (filterEmployees) => {
+        {
+          const {
+            employeeLastName,
+            jobRole,
+            NIC,
+            companyEmail,
+            status,
+            jobType,
+            candidateID,
+            createdAt,
+            updatedAt,
+            phoneNumber,
+            profilePic,
+            streetNo,
+            birthday,
+            city,
+            teamID,
+            ...others
+          } = filterEmployees.toObject();
+          lastSeenArr.push(others);
+          // console.log(others);
+        }
+      })
+    );
+
+    res.send(lastSeenArr);
+  } catch (err) {
+    res.status(404).json({
+      message: "Can't display lastseen of employees",
+      err: err.message,
+    });
+  }
+};
