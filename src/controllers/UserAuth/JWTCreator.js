884aa8189210479c1c6b75a45ef5021712dfa430
kusham <kusham1998@gmail.com>
2022-03-09 10:36:27 +0530

    complete authorization and authantication

--- src/controllers/UserAuth/JWTCreator.js
+++ src/controllers/UserAuth/JWTCreator.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
-const refreshTokens = [];
+
 const createAccessToken = (employeeID, jobRole) => {
   return jwt.sign(
     { id: employeeID, jobRole: jobRole },
     process.env.ACCESS_TOKEN_SECRET_KEY,
     {
-      expiresIn: "15min",
+      expiresIn: "300s",
     }
   );
 };
@@ -15,9 +15,9 @@ const createRefreshToken = (employeeID, jobRole) => {
     { id: employeeID, jobRole: jobRole },
     process.env.REFRESH_TOKEN_SECRET_KEY,
     {
-      expiresIn: "1y",
+      expiresIn: "365d",
     }
   );
 };
 
-module.exports = { createAccessToken, createRefreshToken, refreshTokens };
+module.exports = { createAccessToken, createRefreshToken };
