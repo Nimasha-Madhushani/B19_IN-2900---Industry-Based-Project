66068a124497c31b4deb2e08b4bee7c2d8cc9db3
IKNManimendra <manimendraikn.19@uom.lk>
2022-06-06 14:27:17 +0530

    added exam scheduling controllers

---
+++ src/controllers/promotionSection/examControllers.js
@@ -0,0 +1,69 @@
+const Exam = require("../../models/PromotionModule/Exam");
+
+//controller for HR to schedule exams
+exports.scheduleExam = async (req, res) => {
+
+    const DateCreated = new Date().toLocaleString('IST', { timeZone: 'Asia/Kolkata' });
+
+    try {
+        const eid = req.params.EmployeeID;
+        const { ExamID, ExamName, DateScheduled, JobRole, PaperID } = req.body;
+        const newExam = new Exam({ organizerID: eid, ExamID, ExamName, DateCreated, DateScheduled, PaperID, JobRole });
+        console.log(newExam);
+        await newExam.save();
+        if (!newExam) {
+            return res.status(400).json({ message: "New Exam not created.", success: false });
+        }
+        return res.status(200).json({ message: "successfully created new Exam.", success: true });
+    } catch (error) {
+        return res.status(400).json({ error: error.message, success: false })
+    }
+}
+
+// //delete scheduled exam
+exports.deleteScheduledExam = async (req, res) => {
+    const examId = req.params.ExamID;
+    try {
+        const deleteExam = await Exam.findOneAndDelete({ ExamID: examId });
+        if (!deleteExam) {
+            return res.status(400).json({ message: "Scheduled exam not found", error: error.message });
+        }
+        return res.status(200).json({ message: "Scheduled exam has deleted successfully" })
+    }
+    catch (error) {
+        return res.status(404).json({ message: "Scheduled exam not deleted", error: error.message });
+    }
+}
+
+
+//update scheduled exam
+exports.updateExamDetails = async (req, res) => {
+    const examId = req.params.ExamID;
+    try {
+        const { ExamID, ExamName, DateScheduled, JobRole, PaperID } = req.body;
+
+        let exam = await Exam.findOneAndUpdate({ ExamID: examId }, {
+            ExamID: examId, ExamName: ExamName, DateScheduled: DateScheduled, JobRole: JobRole, PaperID: PaperID
+        });
+        if (!exam) {
+            return res.status(404).json({ message: "not found exam", success: false, error: error.message });
+        }
+        return res.status(200).json({ message: "updated exam", success: true });
+
+    } catch (error) {
+        return res.status(404).json({ success: false, error: error.message });
+    }
+}
+
+//viewScheduled exmas
+exports.viewAllExams = async (req, res) => {
+    try {
+        const examList = await Exam.find();
+        if (!examList) {
+            return res.status(404).json({ message: "Exam list not found", error: error.message, success: false });
+        }
+        return res.status(200).json(examList);
+    } catch (error) {
+        return res.status(400).json({ message: "Error", error: error.message });
+    }
+}
