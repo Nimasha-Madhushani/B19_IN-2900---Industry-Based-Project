e75655bf7a00d107eaec2dc7c2b959970e0e801f
Rashmi Gamage <wgrashmi98@gmail.com>
2022-06-14 07:27:31 +0530

    added validation

--- src/controllers/LeaveSection/mailHandler.js
+++ src/controllers/LeaveSection/mailHandler.js
@@ -1,52 +1,113 @@
 const nodemailer = require("nodemailer");
-const googleapis = require("googleapis");
+const handlebars = require("handlebars");
+const path = require("path");
+const fs = require("fs");
 
+const EMAIL = process.env.EMAIL;
+const APP_PASSWORD = process.env.PASSWORD;
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-const USER = process.env.USER;
-
-const oAuth2Client = new googleapis.google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-const sendEmails = async (
-  employee,
-  data,
-  teamLeader,
-  condition
-) => {
+const sendEmails = async (employee, data, teamLeader, condition) => {
   try {
-    const accessToken = await oAuth2Client.getAccessToken();
+    const emailTemplateSourceRequest = fs.readFileSync(
+      path.join(__dirname, "/main.handlebars"),
+      "utf8"
+    );
+    const emailTemplateSourceCancel = fs.readFileSync(
+      path.join(__dirname, "/cancel.handlebars"),
+      "utf8"
+    );
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+    const emailTemplateSourceApprove = fs.readFileSync(
+      path.join(__dirname, "/approve.handlebars"),
+      "utf8"
+    );
+
+    const emailTemplateSourceReject = fs.readFileSync(
+      path.join(__dirname, "/reject.handlebars"),
+      "utf8"
+    );
+
+    const templateRequest = handlebars.compile(emailTemplateSourceRequest);
+    const templateCancel = handlebars.compile(emailTemplateSourceCancel);
+    const templateApprove = handlebars.compile(emailTemplateSourceApprove);
+    const templateReject = handlebars.compile(emailTemplateSourceReject);
+
+    const htmlToSendRequest = templateRequest({
+      reason: data.reason,
+      employeePhoneNumber:employee.phoneNumber,
+      employeeEmail:employee.companyEmail,
+      employeeID:employee.employeeID,
+      employeeName: employee.employeeFirstName+" "+employee.employeeLastName,
+      task: condition.task,
+      leaveType: data.leaveType,
+      teamLeader: teamLeader.employeeFirstName,
+    });
+    const htmlToSendCancel = templateCancel({
+      reason: data.reason,
+      employeeID:employee.employeeID,
+      employeeName: employee.employeeFirstName+" "+employee.employeeLastName,
+      task: condition.task,
+      leaveType: data.leaveType,
+      startDate: new Date(data.leaveStartDate).toDateString(),
+      appliedReason: data.prevReason, 
+      endDate: new Date(data.leaveEndDate).toDateString(),
+      teamLeader: teamLeader.employeeFirstName,
+    });
+    const htmlToSendApprove = templateApprove({
+      employee: employee.employeeFirstName,
+      task: condition.task,
+      leaveType: data.leaveType,
+      startDate: new Date(data.startDate).toDateString(),
+      endDate: new Date(data.endDate).toDateString(),
+      teamLeaderID: teamLeader.employeeID,
+      teamLeaderName: teamLeader.employeeFirstName+" "+teamLeader.employeeLastName, 
+    });
+    const htmlToSendReject = templateReject({
+      reason: data.reason,
+      employee: employee.employeeFirstName,
+      task: condition.task,
+      leaveType: data.leaveType,
+      startDate: new Date(data.startDate).toDateString(),
+      endDate: new Date(data.endDate).toDateString(), 
+      teamLeaderID: teamLeader.employeeID,
+      teamLeaderName: teamLeader.employeeFirstName+" "+teamLeader.employeeLastName,
+    });
+   
+    const transporter = nodemailer.createTransport({ 
+      service: "gmail", 
       auth: {
-        type: "OAuth2",
-        user: USER,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: EMAIL, 
+        pass: APP_PASSWORD, 
       },
     });
+
     
+
     const mailOptions = {
-      to: condition.teamLeaderBoolean ? employee.companyEmail : teamLeader.companyEmail,
-      subject: condition.teamLeaderBoolean ? condition.task: condition.task + " "+data.leaveType+ " " + "leave",
+      to: condition.teamLeaderBoolean
+        ? employee.companyEmail
+        : teamLeader.companyEmail,
+      subject: condition.teamLeaderBoolean
+        ? condition.task
+        : condition.task + " " + data.leaveType + " " + "leave",
+
       text: data.reason,
-      html: "<h2>request a leave</h2>",
+
+      html:
+        condition.task == "request"
+          ? htmlToSendRequest
+          : condition.task == "cancel"
+          ? htmlToSendCancel
+          : condition.task == "Approved"
+          ? htmlToSendApprove
+          : htmlToSendReject,
+      
     };
 
     const result = await transporter.sendMail(mailOptions);
     return result;
   } catch (error) {
+    console.log(error);
     return error;
   }
 };
