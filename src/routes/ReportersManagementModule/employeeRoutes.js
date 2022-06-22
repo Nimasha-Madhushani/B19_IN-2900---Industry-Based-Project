0c14c3be9a7b3adbf653c3731f5e328ffb6aa130
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-06-22 09:59:40 +0530

    minor changes

--- src/routes/ReportersManagementModule/employeeRoutes.js
+++ src/routes/ReportersManagementModule/employeeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const userRoles = require("../../Config/UserRoles");
 const {
   createEmployee,
   viewEmployees,
@@ -37,9 +37,84 @@ const {
   createOragnizationStructure,
   updateOragnizationStructure,
   getOrganizationStructure,
-  getLevels
+  getLevels,
 } = require("../../controllers/ReportersSection/organizationStructureContollers");
- 
+
+const verify = require("../../middleware/VerifyJWT");
+const verifyRoles = require("../../middleware/verifyUserRole");
+
+// //----------------employee controller------------------------------------------
+
+// router.get("/count", verifyRoles([userRoles.HR]), countEmployees);
+
+// router.get("/user/:id", verify, getUser);
+
+// router.get("/organization", getEmployeesForJobRoles);
+
+// router.get("/organizationStructure", getOrganizationStructure);
+
+// router.get(
+//   "/candidateData",
+//   verifyRoles([userRoles.HR]),
+//   candidatesWithoutProfile
+// );
+
+// router.get("/", viewEmployees);
+
+// router.get("/getall", getallEmployees);
+
+// router.get("/get", getEmployees);
+
+// router.post("/add", verifyRoles([userRoles.HR]), createEmployee);
+
+// router.put("/update/:id", verify, updateEmployeeProfile); //update own profile(NIC disable)
+
+// router.put("/update/employee/:id", updateEmployeeProfile); //only HR can access
+
+// //----------------------teamController------------------------------------------
+
+// router.get("/viewTeam", viewTeam);
+
+// router.get("/getTeam", getTeam);
+
+// router.post("/teamAdd", verifyRoles([userRoles.HR]), addTeam);
+
+// router.put(
+//   "/updateTeam/:id",
+//   verifyRoles([userRoles.HR, userRoles.TeamLeader]),
+//   updateTeam
+// );
+
+// //------------------------productController---------------------------------------
+
+// router.post("/addProduct", verifyRoles([userRoles.HR]), addProduct);
+
+// router.put("/updateProduct/:id", verifyRoles([userRoles.HR]), updateProduct);
+
+// router.get("/viewProducts", viewProducts);
+
+// //--------recent section--------------------------------------------------------
+
+// router.get("/recentSection", recentSection);
+
+// //----------last seen----------------------------------------------------------
+
+// router.get("/lastSeen", displayLastSeen);
+
+// //----------organization strucutre----------------------------------------------
+
+// router.post("/organization/create", createOragnizationStructure);
+
+// router.put("/updateOrganization/:id", updateOragnizationStructure);
+
+// router.get("/levels", getOrganizationStructure);
+
+// router.get("/getLevels", getLevels);
+
+
+
+
+
 
 //----------------employee controller------------------------------------------
 
@@ -48,6 +123,8 @@ router.get("/count",countEmployees)
 router.get("/user/:id", getUser);
 
 router.get("/organization", getEmployeesForJobRoles);
+
+router.get("/organizationStructure", getOrganizationStructure);
 
 router.get("/candidateData", candidatesWithoutProfile);
 
